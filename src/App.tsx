2760e39cfd86a67857a13c78c9ce1be837ace108
choon <chachooon@gmail.com>
2021-02-06 15:16:55 +0900

    add scss

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,7 @@
 import React from "react";
-import { BrowserRouter, Switch, Route, Redirect } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "./ducks/store";
 import TopAlbums from "./pages/TopAlbums";
-import "./app.scss";
 
 export interface MessageProps {
   message: string;
